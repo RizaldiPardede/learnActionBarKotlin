38779fab30b43737c6fded986bdaa04dbcfcd25f
RizaldiPardede <rizaldipardede@gmail.com>
2024-08-06 14:30:39 +0700

    belajar actionbar

---
+++ app/src/main/java/com/example/actionbar/MenuActivity.kt
@@ -0,0 +1,17 @@
+package com.example.actionbar
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+
+class MenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+
+}
